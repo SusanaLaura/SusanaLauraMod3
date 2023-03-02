577212ecb8e36bd3e9d09f1ae3613486e2d071bc
susanalaura <susi.laura2011@gmail.com>
2023-03-02 14:58:51 -0400

    SusanaLauraMod3

---
+++ 01-ts-Fundamentos/Lab1.ts
@@ -0,0 +1,35 @@
+Test('array of string', () =>{
+    const a= "hello";
+    const splitted = a.split("",5);
+    expect (splitted).toEqual(['h','e','l','l','o'])
+})
+
+// Colecion de datos
+let array1: string[] = 'hello';
+array1.push(hello);
+console.log(array1);
+
+
+// Colecion de datos
+let array1: number[] = [1, 2 , 3];
+array1.push(99);
+console.log(array1);
+
+// string
+const str1 = "hello";
+const str2 = '112';
+const str3 = `a
+h
+   e
+     l`;
+console.log(str3);str2
+
+let array1: const str1 = "hello";
+console.log(str1);
+array1.push(99);
+console.log(array1);
+
+const str1 = "hello";
+console.log(str1)
+
+let saludo: string[] = ['hello'] 
